97a550dd287b4c33b97ff5f178641beaeb3f1b4b
Akhil Palla <akhilpalla247@gmail.com>
2025-05-01 17:08:09 +0530

    Update __init__.py

--- chains/__init__.py
+++ chains/__init__.py
@@ -5,9 +5,10 @@ This package contains classes for creating and configuring
 retrieval chains and RAG components.
 """
 from chains.retriever import create_history_aware_retriever_chain
-from chains.rag_chain import create_conversational_rag_chain
+from chains.rag_chain import create_conversational_rag_chain, display_retrieved_context
 
 __all__ = [
     'create_history_aware_retriever_chain',
-    'create_conversational_rag_chain'
+    'create_conversational_rag_chain',
+    'display_retrieved_context'
 ]
