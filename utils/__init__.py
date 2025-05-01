5aef939f2d4ec1236565ded3465c114c2de03365
Akhil Palla <akhilpalla247@gmail.com>
2025-05-01 17:16:00 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -9,10 +9,26 @@ from utils.session_manager import (
     get_session_history,
     clear_session_history
 )
+from utils.user_auth import (
+    initialize_auth_state,
+    login_user,
+    register_user,
+    logout_user,
+    save_user_settings,
+    load_user_settings,
+    render_auth_ui
+)
 
 __all__ = [
     'process_uploaded_files',
     'initialize_session_state',
     'get_session_history',
-    'clear_session_history'
+    'clear_session_history',
+    'initialize_auth_state',
+    'login_user',
+    'register_user',
+    'logout_user',
+    'save_user_settings',
+    'load_user_settings',
+    'render_auth_ui'
 ]
