62b8814e7ea52484d93c2a34b1b87c9bf1ef2048
Akhil Palla <akhilpalla247@gmail.com>
2025-05-01 17:10:20 +0530

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -5,13 +5,43 @@ This package contains UI components for the Streamlit interface.
 """
 from components.sidebar import render_sidebar
 from components.settings_display import display_settings, display_personalization_details
-from components.chat_interface import display_chat_history, display_chat_input, display_assistant_response
+from components.chat_interface import (
+    display_chat_history, 
+    display_chat_history_in_tab,
+    display_chat_input, 
+    display_assistant_response
+)
+from components.dynamic_persona import (
+    render_persona_management, 
+    initialize_persona_state, 
+    save_current_persona, 
+    load_persona,
+    set_mood_override,
+    get_current_persona_with_mood,
+    inject_dynamic_persona_instructions
+)
+from components.document_analysis import (
+    display_document_analysis,
+    analyze_document_metadata,
+    analyze_document_content
+)
 
 __all__ = [
     'render_sidebar',
     'display_settings',
     'display_personalization_details',
     'display_chat_history',
+    'display_chat_history_in_tab',
     'display_chat_input',
-    'display_assistant_response'
+    'display_assistant_response',
+    'render_persona_management',
+    'initialize_persona_state',
+    'save_current_persona',
+    'load_persona',
+    'set_mood_override',
+    'get_current_persona_with_mood',
+    'inject_dynamic_persona_instructions',
+    'display_document_analysis',
+    'analyze_document_metadata',
+    'analyze_document_content'
 ]
